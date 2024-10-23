3ac7c0ef11c32f4e8500df8589780402edc83054
Aaron <reruaarr@gmail.com>
2024-10-23 12:13:49 -0600

    All Browsers Functionality Ok

--- user/src/App.js
+++ user/src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter } from "react-router-dom";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import Navigation from "./Navigation";
 import { AuthProvider } from "./Components/AuthContext";
 import { ToastContainer } from "react-toastify";
@@ -8,14 +7,12 @@ import "./App.css";
 
 function App() {
   return (
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-      <BrowserRouter>
-        <AuthProvider>
-          <Navigation />
-          <ToastContainer />
-        </AuthProvider>
-      </BrowserRouter>
-    </GoogleOAuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <Navigation />
+        <ToastContainer />
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
